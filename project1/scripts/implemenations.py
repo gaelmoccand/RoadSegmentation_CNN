bb4b7e7c5a8681e2d777883890ace911c2828cfe
PBienz <pascal.bienz@sicpa.com>
2017-10-12 13:37:33 +0200

    gradient descend & MSE

--- project1/scripts/implemenations.py
+++ project1/scripts/implemenations.py
@@ -2,31 +2,82 @@
 """Useful methods for project 1"""
 import numpy as np
 
-def least squares GD(y, tx, initial_w, max_iters, gamma):
+# ***************************************************
+# COSTS
+# ***************************************************
+def compute_mse(y, tx, w):
+    """Calculate the loss using the mse"""
+	e = y - tx.dot(w)
+	N = len(e)
+    mse = e.dot(e)/(2*N)
+	
+    return mse
+
+# ***************************************************
+# GRADIENT DESCENT
+# ***************************************************
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+	error = y - tx.dot(w)
+	N = len(error)
+    grad = -tx.T.dot(error)/N
+	
+    return grad, error
+
+
+def gradient_descent(y, tx, initial_w, max_iters, gamma):
+    """Gradient descent algorithm."""
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+	    # Compute gradient and loss
+		grad, error = compute_gradient(y, tx, w)
+        loss = compute_mse(error)
+        # Update w by gradient
+		w = w-gamma*grad
+        # store w and loss
+        ws.append(w)
+        losses.append(loss)
+        print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+
+    return losses, ws
+
+# ***************************************************
+# LEAST SQUARES
+# ***************************************************	
+def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Linear regression using gradient descent"""
     raise NotImplementedError
     
 
-def least squares SGD(y, tx, initial_w, max_iters, gamma):
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     """Linear regression using stochastic gradient descent"""
     raise NotImplementedError
     
 
-def least squares(y, tx):
+def least_squares(y, tx):
     """Least squares regression using normal equations"""
-    raise NotImplementedError
+	a = tx.T.dot(tx)
+    b = tx.T.dot(y)
+	# solve linear system using the QR decomposition
+    return np.linalg.solve(a, b)
     
-    
-def ridge regression(y, tx, lambda ):
+# ***************************************************
+# REGRESSION
+# ***************************************************
+def ridge_regression(y, tx, lambda ):
     """Ridge regression using normal equations"""
     raise NotImplementedError
 
     
-def logistic regression(y, tx, initial_w, max_iters, gamma):
+def logistic_regression(y, tx, initial_w, max_iters, gamma):
     """Logistic regression using gradient descent or SGD"""
     raise NotImplementedError
 
 
-def reg logistic regression(y, tx, lambda_, initial_w, max_iters, gamma):
+def reg_logistic regression(y, tx, lambda_, initial_w, max_iters, gamma):
     """Regularized logistic regression using gradient descent or SGD"""    
     raise NotImplementedError
