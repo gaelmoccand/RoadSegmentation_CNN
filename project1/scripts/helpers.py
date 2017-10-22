9c505773e99dcc3622ab2b17303243adff57b4ab
gaelmoccand <gael.moccand@gmail.com>
2017-10-22 11:45:55 +0200

    add sample data and modify standardize

--- project1/scripts/helpers.py
+++ project1/scripts/helpers.py
@@ -30,11 +30,30 @@ def load_data(sub_sample=True, add_outlier=False):
     return height, weight, gender
 
 
+def sample_data(y, x, seed, size_samples):
+    """sample from dataset."""
+    np.random.seed(seed)
+    num_observations = y.shape[0]
+    random_permuted_indices = np.random.permutation(num_observations)
+    y = y[random_permuted_indices]
+    x = x[random_permuted_indices]
+    return y[:size_samples], x[:size_samples]
+
+
 def standardize(x):
     """Standardize the original data set."""
-    mean_x = np.mean(x)
+    mean_x = np.mean(x, axis=0)
     x = x - mean_x
-    std_x = np.std(x)
+    std_x = np.std(x, axis=0)
+    x = x / std_x
+    return x, mean_x, std_x
+
+def standardizeNan(x):
+    """Standardize the original data set."""
+    x[np.where(x == -999)] = np.nan
+    mean_x = np.nanmean(x,axis=0)
+    x = x - mean_x
+    std_x = np.nanstd(x,axis=0)
     x = x / std_x
     return x, mean_x, std_x
 
