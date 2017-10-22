1b3c5a0c8d281c5272dbaf4b41b92fa713c1842c
gaelmoccand <gael.moccand@gmail.com>
2017-10-22 11:54:09 +0200

    add sgd

--- project1/scripts/implementations.py
+++ project1/scripts/implementations.py
@@ -61,7 +61,20 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     """Linear regression using stochastic gradient descent"""
-    raise NotImplementedError
+    # Define parameters to store w and loss
+    ws = [initial_w]
+    losses = []
+    w = initial_w
+    for n_iter in range(max_iters):
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, 1): # set batch size to 128
+            grad,_=compute_gradient(minibatch_y,minibatch_tx,w) # compute the stochastic gradient using the minibatches
+            w = w - (gamma)*grad # update the w
+            loss = compute_loss(y,tx,w)# compute the loss using the entire sets
+            ws.append(w)#save w
+            losses.append(loss) #save the loss
+        print("Stochastic Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))    
+    return losses, ws
     
 
 def least_squares(y, tx):
